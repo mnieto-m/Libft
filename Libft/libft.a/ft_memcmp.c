c36861b983ba68431473bd8b130b7b556b89b465
mnieto-m <158509540+mnieto-m@users.noreply.github.com>
2024-02-13 01:52:56 +0100

    Actualizar ft_memcmp.c

--- Libft/libft.a/ft_memcmp.c
+++ Libft/libft.a/ft_memcmp.c
@@ -14,18 +14,18 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	size_t	i;
-	unsigned char str1 ;
-	unsigned char str2 ;
+	size_t	 i;
+	unsigned char *str1 ;
+	unsigned char *str2 ;
 	
 	i = 0;
-	str1 = *(unsigned char *) s1;
-	str2 = *(unsigned char *) s2;
+	str1 = (unsigned char *) s1;
+	str2 = (unsigned char *) s2;
 	while(i < n)
 	{
-		if((str1 + i) != (str2 + i))
+		if((str1[i]) != (str2 [i]))
 		{
-			return((str1 + i) - (str2 + i));
+			return((str1[i]- (str2[i]));
 		}
 		i++;
 	}
