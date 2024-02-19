32cf829dd9b5f73f54a5e2e07e4d747a524442b2
mnieto-m <marioa_98@hotmail.com>
2024-02-19 18:52:39 +0100

    aaaaa

--- Libft/ft_substr.c
+++ Libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: mnieto-m <mnieto-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/16 17:49:14 by mnieto-m          #+#    #+#             */
-/*   Updated: 2024/02/17 20:52:10 by mnieto-m         ###   ########.fr       */
+/*   Updated: 2024/02/19 18:52:34 by mnieto-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,18 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char *dst;
 	size_t i;
+	size_t j;
 
-	
 	while (i == (start + i))
 	{
 		if (i == len)
 		{
+			dst = '\0';
 			return (dst);
 		}
+		dst[i] = s[j];
 		i++;
+		j++;
 	}
+	return(0);
 }
