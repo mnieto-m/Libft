2c2aabe2954fd9ba6aaf2f7bd034a371ecabbe41
mnieto-m <marioa_98@hotmail.com>
2024-02-22 21:29:54 +0100

    trim lindo

--- Libft/libft.a/ft_strtrim.c
+++ Libft/libft.a/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: mnieto-m <mnieto-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/22 17:41:50 by mnieto-m          #+#    #+#             */
-/*   Updated: 2024/02/22 19:47:34 by mnieto-m         ###   ########.fr       */
+/*   Updated: 2024/02/22 21:29:30 by mnieto-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,23 @@ char	*ft_strtrim(char const *s1, char const *set)
 {
 	char *dst;
 	size_t i;
-	size_t j;
-	size_t k;
+	size_t size;
+	size_t final;
 
-	i = 0;
-	j = ft_strlen(s1);
-	while (i < j && ft_strchr(set, s1[i]))
-		i++;
-	while (j > i && ft_strchr(set, s1[j - 1]))
-		j--;
-	k = j - i;
-	dst = (char *)malloc(k + 1);
-	if (!dst)
-		return (NULL);
-	ft_substr(s1, i, k);
-	dst[k] = '\0';
+	dst = NULL;
+	if(s1 != NULL && set != NULL)
+	{
+		i = 0;
+		size = ft_strlen(s1);
+		while (s1[i] && ft_strchr(set, s1[i]))
+			i++;
+		while ( s1[size -1] && size > i && ft_strrchr(set, s1[size - 1]))
+			size--;
+		final = size - i;
+		dst = (char *)malloc(final + 1);
+		if (!dst)
+			return (NULL);
+		ft_strlcpy(dst, s1 + i, final + 1);
+	}
+	return (dst);
 }
