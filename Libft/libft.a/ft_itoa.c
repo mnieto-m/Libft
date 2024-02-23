37911bf3b93301865f2aad5ecf85553a981e860d
mnieto-m <marioa_98@hotmail.com>
2024-02-23 15:10:00 +0100

    atoi corregido

--- Libft/libft.a/ft_itoa.c
+++ Libft/libft.a/ft_itoa.c
@@ -1,36 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mnieto-m <mnieto-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/26 11:27:34 by mnieto-m          #+#    #+#             */
-/*   Updated: 2024/02/23 15:06:46 by mnieto-m         ###   ########.fr       */
+/*   Created: 2024/02/23 12:52:54 by mnieto-m          #+#    #+#             */
+/*   Updated: 2024/02/23 12:55:33 by mnieto-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
-int	ft_atoi(const char *str)
+char *ft_itoa(int n)
 {
-	long long int	dest;
-	int				sign;
-
-	dest = 0;
-	sign = 1;
-	while (*str == ' ' || (*str >= 9 && *str <= 13))
-		str++;
-	if (*str == '-' || *str == '+')
-	{
-		if (*str == '-')
-			sign = sign * (-1);
-		str++;
-	}
-	while (*str >= '0' && *str <= '9')
-	{
-		dest = (dest * 10) + *str - 48;
-		str++;
-	}
-	return (dest * sign);
+	int		i;
+	int		len;
+	char	*str;
 }
