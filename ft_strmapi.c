efc6b02338f19589916984d9040fe7ef4ed9fb86
mnieto-m <marioa_98@hotmail.com>
2024-02-23 18:53:10 +0100

    itoaaaaaaaaaaaaaaa mapi ta facil

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -1,41 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mnieto-m <mnieto-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/23 12:52:54 by mnieto-m          #+#    #+#             */
-/*   Updated: 2024/02/23 18:15:42 by mnieto-m         ###   ########.fr       */
+/*   Created: 2024/02/23 18:16:07 by mnieto-m          #+#    #+#             */
+/*   Updated: 2024/02/23 18:52:44 by mnieto-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-static ft_len(int *aux, int *len)
-{	
 
-	while (*aux / 10 != '\0')
-	{
-		len++;
-		aux = *aux /10;
-	}
-	while ()
-	{
-		/* code */
-	}
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
 	
 }
-char *ft_itoa(int n)
-{
-	int		sign;
-	int		len;
-	int		aux;s
-	char	*str;
-	
-	aux = n;
-	len = 0;
-	str = (char *)malloc(len + 1);
-	if (!str)
-		return (0);
-	
-}	
