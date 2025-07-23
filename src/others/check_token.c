b94b0f53dd456d46476516d8d01a929bba933512
mnieto-m <mnieto-m>
2025-07-23 13:36:39 +0200

    checktokens ok

--- src/others/check_token.c
+++ src/others/check_token.c
@@ -6,7 +6,7 @@
 /*   By: mario <mario@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 16:54:33 by mnieto-m          #+#    #+#             */
-/*   Updated: 2025/07/23 13:17:42 by mario            ###   ########.fr       */
+/*   Updated: 2025/07/23 13:33:51 by mario            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,18 +29,9 @@ int	check_token(char *s, char c)
 		if (s[i] != c)
 		{
 			if((s[i + 1] == c || s[i + 1] == '\0')&& s[i] != '\n')
-			{
-				
-				printf("Posicion actual de la string:%s\n",&s[i]);
-				printf("count antes de iterar:%i\n",count);
 				count++;
-				printf("count despues de iterar:%i\n",count);
-			}	
-			
 		}
 		i++;
-		printf("Este es el valor de count al final de bucle:%i\n",count);
 	}
-	printf("count:%i\n",count);
 	return (count);
 }
