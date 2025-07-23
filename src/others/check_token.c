9e9c072561da0b258347e16020af280c861c9eae
mnieto-m <mnieto-m>
2025-07-23 13:36:15 +0200

    check_tokens now correct pecfect colums

--- src/others/check_token.c
+++ src/others/check_token.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   check_token.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mnieto-m <mnieto-m@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mario <mario@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 16:54:33 by mnieto-m          #+#    #+#             */
-/*   Updated: 2025/06/03 17:29:09 by mnieto-m         ###   ########.fr       */
+/*   Updated: 2025/07/23 13:17:42 by mario            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,28 @@ int	check_token(char *s, char c)
 
 	i = 0;
 	count = 0;
-	while (s[i] != '\0')
+	while (s[i] == c) 
+    {
+        i++;
+    }
+
+	while (s[i])
 	{
-		if (s[i] != c && (s[i + 1] == c || s[i + 1] == '\0'))
-			count++;
+		if (s[i] != c)
+		{
+			if((s[i + 1] == c || s[i + 1] == '\0')&& s[i] != '\n')
+			{
+				
+				printf("Posicion actual de la string:%s\n",&s[i]);
+				printf("count antes de iterar:%i\n",count);
+				count++;
+				printf("count despues de iterar:%i\n",count);
+			}	
+			
+		}
 		i++;
+		printf("Este es el valor de count al final de bucle:%i\n",count);
 	}
+	printf("count:%i\n",count);
 	return (count);
 }
